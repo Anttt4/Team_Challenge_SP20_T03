c9162e9b6b7193ad44b6c897d974f9213c9d4450
mariarisco <30744404+mariarisco@users.noreply.github.com>
2025-04-20 20:23:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ def predict():
 
     return jsonify({ # Return the prediction
             # "prediction": int(prediction),
-            "¿contratara el deposito?": label
+            "contratara el deposito?": label
         }) 
 
 if __name__ == '__main__':
